e331bd2b59df7a1f946977fe6b16483db5d0e3cd
IsaiasQB <isaias.qb@hotmail.com>
2023-03-13 20:22:28 -0400

    added the first fetch request

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
-var getUserRepos = function() {
-  fetch("https://api.github.com/users/octocat/repos")
+var getUserRepos = function(user) {
+  var apiUrl = `https://api.github.com/users/${user}/repos`;
+
+  // make a request ot the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data)
+    });
+  });
 };
 
-getUserRepos()
+getUserRepos("isaiasqb")
